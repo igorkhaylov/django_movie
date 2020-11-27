fe4ed9aff8ace05636164af0a85a36f3075b4841
igorworkpc <sonikry.99@gmail.com>
2020-11-27 17:55:51 +0500

    lesson 6 and started lesson7

--- movies/views.py
+++ movies/views.py
@@ -7,5 +7,11 @@ class MoviesView(View):
     """Список фильмов"""
     def get(self, request):
         movies = Movie.objects.all()
-        return render(request, "movies/movie_list.html", {"movie_list": movies})
+        return render(request, "movies/movies.html", {"movie_list": movies})
+
+
+class MovieDetailView(View):
+    def get(self, request, pk):
+        movie = Movie.objects.get(id=pk)
+        return render(request, "movies/movie_detail.html", {"movie": movie})
 

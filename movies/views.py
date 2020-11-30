a6c6d0d983614df650222008e9842334f678b323
igorworkpc <sonikry.99@gmail.com>
2020-11-30 18:00:36 +0500

    changing vievs

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from django.views.generic import ListView
 from .models import Movie
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     """Список фильмов"""
     def get(self, request):
         movies = Movie.objects.all()
@@ -11,7 +12,7 @@ class MoviesView(View):
 
 
 class MovieDetailView(View):
-    def get(self, request, pk):
-        movie = Movie.objects.get(id=pk)
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
         return render(request, "movies/movie_detail.html", {"movie": movie})
 

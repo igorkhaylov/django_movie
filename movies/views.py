94ca30f27d90059763cb6636a724202bde2bd8cd
igorworkpc <sonikry.99@gmail.com>
2020-12-01 17:59:06 +0500

    finished lesson 8

--- movies/views.py
+++ movies/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Movie
 
 
 class MoviesView(ListView):
     """Список фильмов"""
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movies.html", {"movie_list": movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    # template_name = "movies/movie_list.html"
+    # def get(self, request):
+    #     movies = Movie.objects.all()
+    #     return render(request, "movies/movie_list.html", {"movie_list": movies})
 
 
-class MovieDetailView(View):
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, "movies/movie_detail.html", {"movie": movie})
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = "url"
+    # def get(self, request, slug):
+    #     movie = Movie.objects.get(url=slug)
+    #     return render(request, "movies/movie_detail.html", {"movie": movie})
 
